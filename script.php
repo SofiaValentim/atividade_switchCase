f3f1178fb68d0e740e6587d7952e09d29ac8c34d
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-10 18:18:20 -0300

    alteração

--- script.php
+++ script.php
@@ -1,16 +1,28 @@
+<head>
+<STYLE>
+    img {
+        width: 128px;
+    }
+</STYLE>
+
+
+</head>
 <body>
     <div>
 <?php
 $favcolor = $_POST["favcolor"];
 switch ($favcolor){
 case "vermelho":
-    echo "<p> sua cor favorita é vermelho! </p>";
+    echo "<p> ☆ sua cor favorita é vermelho! ☆ </p>";
+    echo "<img src='morango.avif'/>";
 break;
 case "azul";
-echo "<p> sua cor favorita é azul! </p>";
+echo "<p> ☆ sua cor favorita é azul! ☆ </p>";
+echo "<img src='azul.jpg'/>";
 break;
 case "verde";
-echo "<p> sua cor favorita é verde! </p>";
+echo "<p> ☆ sua cor favorita é verde! ☆ </p>";
+echo "<img src='verde.png'/>";
 break;
 default:
 echo "<p> sua cor favorita nâo é vermelho,azul ou verde! </p>";
